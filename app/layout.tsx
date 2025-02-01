43435d781a7119934908670442b5e4c295321f40
Fred Zaw <fred.zaw@gmail.com>
2025-02-01 13:06:47 -0800

    centered layout, fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,12 +25,13 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: '32x32' },
-      { url: '/favicon-32x32.png', sizes: '32x32' },
-      { url: '/favicon-16x16.png', sizes: '16x16' },
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
     ],
+    shortcut: [{ url: '/favicon.ico' }],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180' },
+      { url: '/apple-touch-icon.png' }
     ],
     other: [
       {
